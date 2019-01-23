2282371f710e1ee7774882d9324daaf6bb74ebf5
unknown <yangyutan007@gmail.com>
2019-01-23 11:38:52 +1100

    bug fixed

--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -5,90 +5,99 @@ const app = getApp();
 
 Page({
 
-    // data
-    data: {
-      thumb:'',
-      nickName:'',
-      myTaskFlag:'',
-      faqFlag:'',
-      reportFlag:'',
-      aboutUsFlag:'',
-      modalShow:false
-    },
+  // data
+  data: {
+    thumb: '',
+    nickName: '',
+    myTaskFlag: '',
+    faqFlag: '',
+    reportFlag: '',
+    aboutUsFlag: '',
+    modalShow: false
+  },
 
 
-    onShow() {
-     this.setData({
-       thumb:app.globalData.clientInfo.avatarUrl,
-       nickName:app.globalData.clientInfo.nickName
-     })
-     console.log(this.data)
-     app.globalData.clickFlag = false
-    },
+  onShow() {
+    this.setData({
+      thumb: app.globalData.clientInfo.avatarUrl,
+      nickName: app.globalData.clientInfo.nickName
+    })
+    console.log(this.data)
+    app.globalData.clickFlag = false
+  },
 
-    goToMyTask:function(e) {
-      let self = this
-      if(!app.globalData.clickFlag){
-        app.globalData.clickFlag = true
-        wx.navigateTo({
-          url: '../myTask/myTask',
-        })
-      }
-    },
-
-    goToFAQ:function(){
-      let self = this
-      if (!app.globalData.clickFlag){
-        app.globalData.clickFlag = true
-        wx.navigateTo({
-          url: '../FAQ/FAQ',
-        })
-      }
-    },
-
-    goToReport:function() {
-      let self = this
-      if (!app.globalData.clickFlag){
-        app.globalData.clickFlag = true
-        wx.navigateTo({
-          url: '../report/report',
-        })
-      }
-    },
-
-    goToAboutUs:function() {
-      let self = this
-      if (!app.globalData.clickFlag){
-        app.globalData.clickFlag = true
-        wx.navigateTo({
-          url: '../aboutUs/aboutUs',
-        })
-      }
-
-    },
-
-    hideShareModal:function(){
-      this.setData({
-        modalShow:false
+  goToMyTask: function(e) {
+    let self = this
+    if (!app.globalData.clickFlag) {
+      app.globalData.clickFlag = true
+      wx.navigateTo({
+        url: '../myTask/myTask',
       })
-    },
-    
-    showModal: function(){
-      this.setData({
-        modalShow:true
-      })
-    },
+    }
+  },
 
-    showQRCode: function () {
+  goToFAQ: function() {
+    let self = this
+    if (!app.globalData.clickFlag) {
+      app.globalData.clickFlag = true
+      wx.navigateTo({
+        url: '../FAQ/FAQ',
+      })
+    }
+  },
+
+  goToReport: function() {
+    let self = this
+    if (!app.globalData.clickFlag) {
+      app.globalData.clickFlag = true
+      wx.navigateTo({
+        url: '../report/report',
+      })
+    }
+  },
+
+  goToAboutUs: function() {
+    let self = this
+    if (!app.globalData.clickFlag) {
+      app.globalData.clickFlag = true
+      wx.navigateTo({
+        url: '../aboutUs/aboutUs',
+      })
+    }
+  },
+
+  goToPublish: function() {
+    let self = this
+    if (!app.globalData.clickFlag) {
+      app.globalData.clickFlag = true
+      wx.navigateTo({
+        url: '../publish/publish',
+      })
+    }
+  },
+
+  hideShareModal: function() {
+    this.setData({
+      modalShow: false
+    })
+  },
+
+  showModal: function() {
     this.setData({
       modalShow: true
     })
   },
 
-  showImg: function (e) {
+  showQRCode: function() {
+    this.setData({
+      modalShow: true
+    })
+  },
+
+  showImg: function(e) {
     let self = this
     wx.previewImage({
-      urls:['../../images/icons/customer.jpg']
+      urls: ['../../images/icons/customer.jpg']
     })
   },
 
@@ -101,7 +110,7 @@ Page({
           title: '复制成功',
         })
       },
-      fail(){
+      fail() {
         wx.showToast({
           title: '复制失败',
         })
